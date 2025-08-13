209df04c98839dccc2c2654c8da3a853e971060a
Anup Toggi <89697446+Anup-Toggi@users.noreply.github.com>
2025-08-13 18:41:17 +0530

    Created tests.py

---
+++ tests.py
@@ -0,0 +1,22 @@
+from color_pair import (get_color_from_pair_number,get_pair_number_from_color)
+from print_manual import print_color_code_manual
+
+def test_number_to_pair(pair_number, expected_major_color, expected_minor_color):
+    major_color, minor_color = get_color_from_pair_number(pair_number)
+    assert major_color == expected_major_color, f"Expected {expected_major_color}, got {major_color}"
+    assert minor_color == expected_minor_color, f"Expected {expected_minor_color}, got {minor_color}"
+
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
+    pair_number = get_pair_number_from_color(major_color, minor_color)
+    assert pair_number == expected_pair_number, f"Expected {expected_pair_number}, got {pair_number}"
+
+if __name__ == '__main__':
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('All tests passed.\n')
+
+    print('Color Code Reference Manual:')
+    print(print_color_code_manual())
